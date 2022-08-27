6ad7fd398b11c0d38604c1bddb1465e564b3600c
aicherish <aicherish0320@163.com>
2022-08-27 10:08:34 +0800

    vite basic 下

--- vite-basic/src/App.vue
+++ vite-basic/src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>
-    <img src="./assets/lisa3.png" alt="lisa">
-    Hello Vite
-    </div>
+  <div id="app">
+    <!-- <img src="./assets/lisa3.png" alt="lisa" /> -->
+    <!-- <img :src="imgUrl" alt="lisa" /> -->
+    <HelloWorld msg="Vue3 + Vite"></HelloWorld>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+// import imgUrl from './assets/lisa3.png'
+</script>
 
-<style lang="scss" scoped></style>
+<style>
+#app {
+  height: 100vh;
+  /* background-image: url('./assets/lisa3.png'); */
+  /* background-repeat: no-repeat; */
+}
+</style>
---
+++ vite-basic/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>
+    <div>{{msg}}</div>
+    <div class="box"></div>
+  </p>
+</template>
+
+<script setup>
+defineProps(['msg'])
+// import style from './HelloWorld.module.css'
+// console.log('style >>> ', style)
+</script>
+
+<!-- <style module>
+p {
+  color: red;
+}
+.font {
+  font-size: 60px;
+}
+</style> -->
+
+<style lang="scss" scoped>
+$color: pink;
+p {
+  color: $color;
+}
+.box  {
+  width: 100px;
+  height: 50px;
+  background: cyan;
+  transform: translateZ(45deg);
+}
+</style>
