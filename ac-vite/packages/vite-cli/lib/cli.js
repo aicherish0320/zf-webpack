a36e2e4dbcccec39191585f6e7b7af49ae26b78e
aicherish0320 <aicherish0320@163.com>
2022-08-29 12:48:09 +0800

    ac-vite环境搭建

--- ac-vite/packages/vite-cli/lib/cli.js
+++ ac-vite/packages/vite-cli/lib/cli.js
@@ -1,3 +1,29 @@
-'use strict'
+const Koa = require('koa')
+const { serveStaticPlugin } = require('./serverPluginServeStatic')
 
-console.log('Hello Vite CLI >>> ')
+/*
+  1. 实现一个 Http 服务器，让客户端访问 index.html 能返回 index.html
+  2. 当访问 /src/main.js 的时候，返回 main.js 的内容
+*/
+
+function createServer() {
+  const app = new Koa()
+  const projectRoot = process.cwd() // 当前的工作路径 current working directory
+
+  const context = {
+    app,
+    projectRoot
+  }
+
+  app.use((ctx, next) => {
+    Object.assign(ctx, context) // 把 context 上的 app 和 projectRoot 属性赋给 ctx
+    return next()
+  })
+
+  const resolvedPlugins = [serveStaticPlugin]
+  resolvedPlugins.forEach((plugin) => plugin(context))
+
+  return app
+}
+
+createServer().listen(3331, () => console.log(3331))
---
+++ ac-vite/packages/vite-cli/lib/serverPluginServeStatic.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const static = require('koa-static')
+
+// 这个插件相当于给应用添加了一个静态文件中间件，以当前的命令选择目录为根目录
+function serveStaticPlugin({ app, projectRoot }) {
+  app.use(static(projectRoot))
+}
+
+exports.serveStaticPlugin = serveStaticPlugin
