afd09fe08014bcd36a89baabdeed1f22f6c58da2
aicherish0320 <aicherish0320@163.com>
2022-09-16 15:21:57 +0800

    实现箭头函数的插件

--- ast/02-arrow.js
+++ ast/02-arrow.js
@@ -5,9 +5,52 @@
 
 const core = require('@babel/core')
 const types = require('babel-types')
-const arrowPlugin = require('babel-plugin-transform-es2015-arrow-functions')
+// const arrowPlugin = require('babel-plugin-transform-es2015-arrow-functions')
 
-const sourceCode = `const sum = (a, b) => { return a + b }`
+// 访问者 迭代器模式 iterator
+const arrowPlugin = {
+  visitor: {
+    ArrowFunctionExpression(nodePath) {
+      const node = nodePath.node
+      hoistFunctionEnvironment(nodePath)
+      node.type = 'FunctionExpression'
+    }
+  }
+}
+
+function hoistFunctionEnvironment(fnPath) {
+  const thisEnvFn = fnPath.findParent((p) => {
+    return (p.isFunction() && !p.isArrowFunctionExpression()) || p.isProgram()
+  })
+
+  let thisPaths = getScopeInformation(fnPath)
+  let thisBinding = '_this'
+  let thisIdentifier = types.identifier(thisBinding)
+
+  thisEnvFn.scope.push({
+    type: 'VariableDeclaration',
+    id: thisIdentifier,
+    init: types.thisExpression()
+  })
+
+  if (thisPaths.length > 0) {
+    thisPaths.forEach((thisPath) => {
+      thisPath.replaceWith(thisIdentifier)
+    })
+  }
+}
+
+function getScopeInformation(fnPath) {
+  let thisPaths = []
+  fnPath.traverse({
+    ThisExpression(thisPath) {
+      thisPaths.push(thisPath)
+    }
+  })
+  return thisPaths
+}
+
+const sourceCode = `const sum = (a, b) => { return a + b; console.log(this) }`
 
 const targetCode = core.transform(sourceCode, {
   plugins: [arrowPlugin]
